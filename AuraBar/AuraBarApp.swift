23dbbb8ec71dbc37e4e8dd9cb2b5d009cbbbc630
Aditya Sahu <adityaonx@gmail.com>
2026-05-03 10:42:00 +0530

    Initial Commit

---
+++ AuraBar/AuraBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraBarApp.swift
+//  AuraBar
+//
+//  Created by Content on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
